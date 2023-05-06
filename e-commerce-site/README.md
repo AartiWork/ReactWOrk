82755a9e276d0ba958ecba43fc88d92910b6a703
Aarti <131785557+AartiWork@users.noreply.github.com>
2023-05-07 02:34:51 +0530

    Ecommerce website

---
+++ e-commerce-site/README.md
@@ -0,0 +1,31 @@
+### To intsall all rhe required dependecies
+
+```
+npm install
+```
+
+## Start the project
+
+```
+npm start
+```
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+### Test the application
+
+```
+npm test
+```
+
+Launches the test runner in the interactive watch mode.<br>
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### Build application
+
+```
+npm run build
+```
+
+Builds the app for production to the `build` folder.<br>
---
+++ e-commerce-site/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Switch,Route} from "react-router-dom";
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+import Modal from './components/Modal';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+      <Modal />
+    </React.Fragment>
+  );
+}
+
+export default App;
